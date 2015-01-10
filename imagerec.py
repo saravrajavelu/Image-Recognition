90f01623b0788fbea5732908ef95eedbca1a35f0
sarav <saravrajavelu@gmail.com>
2015-01-11 03:59:11 +0530

    Basic

--- imagerec.py
+++ imagerec.py
@@ -5,10 +5,10 @@ import matplotlib
 
 #Taking the image of a dot in a 8x8
 
-i = Image.open('images/dot.png')
+i = Image.open('images/dotndot.png')
 
 # Returns 3-D array corresponding to the image, [row,column,pixel]
-#Pixel has R,G,B and Alpha
+#Pixel has R,G,B and Alpha(Transparency)
 iar = np.asarray(i)
 
 print(iar)
