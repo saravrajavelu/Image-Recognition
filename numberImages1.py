8a4d397c80b5f7f2477746ac81cca43930fae43f
sarav <saravrajavelu@gmail.com>
2015-01-12 23:33:03 +0530

    thresholdFunction added
    Changes to black and white

--- numberImages1.py
+++ numberImages1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 #Taking the image of a dot in a 8x8
 
-i = Image.open('images/dotndot.png')
+i = Image.open('images/numbers/0.1.png')
 
 # Returns 3-D array corresponding to the image, [row,column,pixel]
 #Pixel has R,G,B and Alpha(Transparency)
